b6d55a997070fcd1b7d4258f8156baabccf2afdb
Evan Wang <evan.wang@canonical.com>
2016-11-02 11:23:42 +0800

    add script for turbo flash

---
+++ turbo-flash.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+fastboot flash bootloader bootloader
+fastboot oem reboot fb
+sleep 3 # wait for bootloader reboot
+fastboot oem cmd "fdisk -c"
+fastboot oem reboot fb
+sleep 5 # wait for bootloader reboot
+fastboot oem poweroff
+echo "bootloader updated."
+echo "start to update other partitions."
+echo "please power your device to fastboot mode manually then press any key to continue..."
+read
+fastboot flash cache cache.img
+fastboot flash ldfw ldfw
+fastboot flash dtb dtb
+fastboot flash bootlogo logo.bin
+fastboot flash bootimg boot.img
+fastboot flash recovery recovery.img
+fastboot flash system ubuntu.sparse.img
+fastboot flash custom custom.img
+fastboot flash userdata userdata.img
+#fastboot -w 
+# if you would like to clear cache/userdata partition, please flash them by image instead.
+fastboot reboot
+echo "device rebooting..."
